4a6007dc216d4d4c68f0bebd66e0c744a0695b9e
bauerbrun0 <bauerbruno@protonmail.com>
2024-02-19 19:06:25 +0100

    feat: add skeleton-ui with custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+import { join } from 'path';
+import { skeleton } from '@skeletonlabs/tw-plugin';
+import { skeletonTheme } from './skeleton-theme';
+
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  darkMode: 'class',
+  content: [
+	'./src/**/*.{html,js,svelte,ts}',
+	join(require.resolve(
+		'@skeletonlabs/skeleton'),
+		'../**/*.{html,js,svelte,ts}'
+	),
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+	skeleton({
+		themes: {
+			custom: [
+				skeletonTheme
+			]
+		}
+	})
+  ],
 }
-
